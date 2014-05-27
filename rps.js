426a37f2366def805050e6644482ab2112a176db
tmart <thomas@internmatch.com>
2014-05-27 09:17:03 -0700

    First commit

---
+++ rps.js
@@ -0,0 +1,42 @@
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} alert("Computer: " + computerChoice);
+
+var compare = function(choice1, choice2) {
+    if(choice1 === choice2) {
+        return"The result is a tie!";
+    }
+    else if(choice1 === "rock") {
+        
+        if(choice2 === "scissors") {
+            return"rock wins";
+        }
+        else {
+            return"paper wins";
+        }
+    }
+    if(choice1 === "paper") {
+        if(choice2 === "rock") {
+            return"paper wins";
+        }
+        else {
+            return "scissors wins";
+        }
+    }
+    if(choice1 === "scissors") {
+        if(choice2 === "rock") {
+            return"rock wins";
+        }
+        else {
+            return"scissors win";
+        }
+    }
+};
+
+alert(compare(userChoice, computerChoice));
